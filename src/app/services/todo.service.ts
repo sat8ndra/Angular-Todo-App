c81cbeaae766d1cdb82c8ff16931fac74326714f
satyendra gupta <sat8ndra@gmail.com>
2019-05-17 09:17:48 +0530

    getTodos service implemented and returning data dynamically

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor(private http:HttpClient) { }
+
+  getTodos() {
+    return [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: true
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: false
+      }
+    ]
+  } 
+}
